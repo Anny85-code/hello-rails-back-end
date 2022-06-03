f7b9a73c8ff7d7aedcf315cbb54da170d74af7d8
uae <aniekaudo@yahoo.com>
2022-06-03 21:03:13 +0100

    :zap: Bug
    Fixing bug

--- app/controllers/Api/v1/greetings_controller.rb
+++ app/controllers/Api/v1/greetings_controller.rb
@@ -7,21 +7,4 @@ class Api::V1::GreetingsController < ApplicationController
 
     render json: @greetings
   end
-
-  def create
-    @greeting = Greeting.new(greeting_params)
-
-    if @greeting.save
-      render json: @greeting, status: :created, location: @greeting
-    else
-      render json: @greeting.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /greetings/1
-  
-  # Only allow a list of trusted parameters through.
-  def greeting_params
-    params.require(:greeting).permit(:title, :message)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :greetings
+      resources :greetings, only: [:index]
     end
   end
 end
